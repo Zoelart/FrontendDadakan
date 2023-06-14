8d55a3d2a4fc47465da43d016f19f3ecf4c4842b
Zoelart <93458096+Zoelart@users.noreply.github.com>
2023-06-14 18:17:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const menu = document.getElementById('menu-label');
+const sidebar = document.getElementsByClassName('sidebar')[0];
 
-<i class="bi bi-list"></i>
-
-
+menu.addEventListener('click', function() {
+    sidebar.classList.toggle('hide');
+    console.log('ok');
+})
